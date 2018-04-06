2679e812c65d9e04dd7109f5f36a63e1381f8b5b
Waxwork1976 <36605084+Waxwork1976@users.noreply.github.com>
2018-04-06 16:55:53 +0200

    Extract data retreival from main script

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,4 @@
-wd <- paste0(getwd(), "/", "peer_w4")
-if (!dir.exists(wd)){
-    dir.create(wd)
-}
 
-setwd(wd)
-
-f = tempfile()
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", f, method = "curl")
-
-unzip(f, overwrite = T, junkpaths =T)
 
 #####################################################################################
 library(data.table)
